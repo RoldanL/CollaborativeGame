ec02c4bb83f015f071b114e8ad4be135fba314f6
carloflojo11 <carloandrey.flojo.m@bulsu.edu.ph>
2024-05-25 22:59:50 +0800

    level

--- RoVenture/Assets/LevelMenu.cs
+++ RoVenture/Assets/LevelMenu.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelMenu : MonoBehaviour
 {
-  public void OpenLevel(int levelId)
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedlevel = PlayerPrefs.GetInt("Unlockedlevel", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i = 0;i < unlockedlevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+    public void OpenLevel(int levelId)
     {
         string levelName = "Level " + levelId;
         SceneManager.LoadScene(levelName);
